20789857c48f4e96720843577cc069ee80b2f8fc
Wayne Lee <waynel1996weiyang@gmail.com>
2021-04-08 16:25:58 -0700

    Add new files

---
+++ passion-project-frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom"
+
+import WelcomePage from "./layouts/WelcomePage"
+import P1HomePage from "./layouts/Puzzle1/HomePage"
+
+function App() {
+  const [progress, setProgress] = useState([])
+
+  const numOfLevel = 1
+
+  useEffect(() => {
+    setDefaultProgress()
+  }, [])
+
+  const setDefaultProgress = () => {
+    let newProgress = []
+    for(var i=progress.length; i<numOfLevel; i++) {
+      newProgress.push(false)
+    }
+    setProgress(newProgress)
+    console.log(newProgress)
+  }
+
+  const updateLocalProgress = (data) => {
+    let newProgress = progress
+    newProgress.splice(data.level-1, 1, true)
+    setProgress(newProgress)
+    console.log(newProgress)
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/puzzle1">
+          <P1HomePage updateLocalProgress={updateLocalProgress} />
+        </Route>
+        <Route path="/">
+          <WelcomePage />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App;
