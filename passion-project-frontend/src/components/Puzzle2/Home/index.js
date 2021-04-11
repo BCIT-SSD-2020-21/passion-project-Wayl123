a94713c86135b24ce470161acaee208ec22c6cc7
Wayne Lee <waynel1996weiyang@gmail.com>
2021-04-10 19:56:08 -0700

    Add transition to trigger change

--- passion-project-frontend/src/components/Puzzle2/Home/index.js
+++ passion-project-frontend/src/components/Puzzle2/Home/index.js
@@ -9,9 +9,9 @@ export default function Home({trigger, checkUnlock}) {
       <p>Now there are site that just require you to find some things</p>
       <p>You need to find some things below to unlock the locked button</p>
       <p>Find the way to turn the light green</p>
-      <span className={`${styles.dot} ${trigger[0] ? styles.unlock : styles.lock}`}></span>
-      <span className={`${styles.dot} ${trigger[1] ? styles.unlock : styles.lock}`}></span>
-      <span className={`${styles.dot} ${trigger[2] ? styles.unlock : styles.lock}`}></span>
+      <span className={`${styles.transition} ${styles.dot} ${trigger[0] ? styles.unlock : styles.lock}`}></span>
+      <span className={`${styles.transition} ${styles.dot} ${trigger[1] ? styles.unlock : styles.lock}`}></span>
+      <span className={`${styles.transition} ${styles.dot} ${trigger[2] ? styles.unlock : styles.lock}`}></span>
       <button onClick={checkUnlock}>Locked button</button>
     </div>
   )
